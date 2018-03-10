d85dae56b3ed49a37fce15e1cd9e0627321c3090
Stanislav Kasprik <stanislav.kasprik@gmail.com>
2018-03-10 17:01:57 +0100

    FOOB-4 Add project template

---
+++ FooBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FooBar
+//
+//  Created by Stanislav Kasprik on 10/03/2018.
+//  Copyright © 2018 Stanislav Kasprik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
